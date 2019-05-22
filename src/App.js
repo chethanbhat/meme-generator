a8ba10fa9906449aed0feb1e676556b2356dbcb3
Chethan Bhat <chethanbhat@gmail.com>
2019-05-22 13:58:00 +0530

    Simple Project Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { Meme } from './components/Meme';
+
+const objectToQueryParam = obj => {
+  const params = Object.entries(obj).map(([key, value]) => `${key}=${value}`);
+  return '?' + params.join('&');
+};
 
 function App() {
+  const [templates, setTemplates] = useState([]);
+  const [template, setTemplate] = useState(null);
+  const [topText, setTopText] = useState('');
+  const [bottomText, setBottomText] = useState('');
+  const [meme, setMeme] = useState(null);
+
+  useEffect(() => {
+    fetch('https://api.imgflip.com/get_memes')
+      .then(res => res.json())
+      .then(result => setTemplates(result.data.memes));
+  }, []);
+
+  if (meme) {
+    return (
+      <div style={{ textAlign: 'center' }}>
+        <img style={{ width: '300px' }} src={meme} alt="My meme" />
+      </div>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ textAlign: 'center' }}>
+      {template && (
+        <>
+          <Meme template={template} />
+          <form
+            onSubmit={async e => {
+              e.preventDefault();
+              const params = {
+                template_id: template.id,
+                username: process.env.REACT_APP_IMGFLIP_USERNAME,
+                password: process.env.REACT_APP_IMGFLIP_PASSWORD,
+                text0: topText,
+                text1: bottomText
+              };
+              // Add logic to create meme from api
+              const response = await fetch(
+                `https://api.imgflip.com/caption_image${objectToQueryParam(
+                  params
+                )}`,
+                {}
+              );
+              const json = await response.json();
+              setMeme(json.data.url);
+            }}
+          >
+            <input
+              placeholder="Top text"
+              value={topText}
+              onChange={e => setTopText(e.target.value)}
+            />
+            <input
+              placeholder="Bottom text"
+              value={bottomText}
+              onChange={e => setBottomText(e.target.value)}
+            />
+            <button type="submit">Create Meme</button>
+          </form>
+        </>
+      )}
+      {!template && (
+        <>
+          <h1>Pick a template</h1>
+          {templates.map(template => {
+            return (
+              <Meme
+                template={template}
+                onClick={() => {
+                  setTemplate(template);
+                }}
+              />
+            );
+          })}
+        </>
+      )}
     </div>
   );
 }
---
+++ src/components/Meme.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Meme = ({ template, onClick }) => {
+  return (
+    <img
+      key={template.id}
+      src={template.url}
+      alt={`${template.name} meme template`}
+      style={{ maxHeight: '300px', maxWidth: '300px' }}
+      onClick={onClick}
+    />
+  );
+};
